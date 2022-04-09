55cb5977755986ad2019f04ce4f176243a956ab4
ravipuniaa <ravipunia010@gmail.com>
2022-04-09 19:39:24 +0530

    Top K Frequent Elements(9April).java

---
+++ Top K Frequent Elements(9April).java
@@ -0,0 +1,39 @@
+// Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,1,1,2,2,3], k = 2
+// Output: [1,2]
+// Example 2:
+
+// Input: nums = [1], k = 1
+// Output: [1]
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// k is in the range [1, the number of unique elements in the array].
+// It is guaranteed that the answer is unique.
+ 
+
+// Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i : nums){ map.put(i, map.getOrDefault(i, 0) + 1); }
+        
+        Queue<Integer> maxheap = new PriorityQueue<>((a, b) -> map.get(b) - map.get(a));
+        for(int key : map.keySet()){maxheap.add(key); }
+        
+        int ans[] = new int[k];
+        for(int i = 0; i < k; i++){
+            ans[i] = maxheap.poll();
+        }
+        
+        return ans; 
+    }
+}
