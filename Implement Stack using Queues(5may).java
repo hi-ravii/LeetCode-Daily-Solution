f1d9f39a8bdd79d5ad8d06d039337bf56fee11e6
Ravi Punia <70934188+ravipuniaa@users.noreply.github.com>
2022-05-05 23:22:18 +0530

    Implement Stack using Queues.java

---
+++ Implement Stack using Queues(5may).java
@@ -0,0 +1,76 @@
+// Implement a last-in-first-out (LIFO) stack using only two queues. The implemented stack should support all the functions of a normal stack (push, top, pop, and empty).
+
+// Implement the MyStack class:
+
+// void push(int x) Pushes element x to the top of the stack.
+// int pop() Removes the element on the top of the stack and returns it.
+// int top() Returns the element on the top of the stack.
+// boolean empty() Returns true if the stack is empty, false otherwise.
+// Notes:
+
+// You must use only standard operations of a queue, which means that only push to back, peek/pop from front, size and is empty operations are valid.
+// Depending on your language, the queue may not be supported natively. You may simulate a queue using a list or deque (double-ended queue) as long as you use only a queue's standard operations.
+ 
+
+// Example 1:
+
+// Input
+// ["MyStack", "push", "push", "top", "pop", "empty"]
+// [[], [1], [2], [], [], []]
+// Output
+// [null, null, null, 2, 2, false]
+
+// Explanation
+// MyStack myStack = new MyStack();
+// myStack.push(1);
+// myStack.push(2);
+// myStack.top(); // return 2
+// myStack.pop(); // return 2
+// myStack.empty(); // return False
+ 
+
+// Constraints:
+
+// 1 <= x <= 9
+// At most 100 calls will be made to push, pop, top, and empty.
+// All the calls to pop and top are valid.
+class MyStack {
+    Queue<Integer> q1;
+    Queue<Integer>q2;
+
+    public MyStack() {
+        q1=new LinkedList<>();
+        q2 = new LinkedList<>();
+    }
+    
+    public void push(int x) {
+        while(!q1.isEmpty()){
+            q2.offer(q1.remove());
+        }
+        q1.offer(x);
+        while(!q2.isEmpty()){
+            q1.offer(q2.remove());
+        }
+    }
+    
+    public int pop() {
+        return q1.remove();
+    }
+    
+    public int top() {
+        return q1.peek();
+    }
+    
+    public boolean empty() {
+        return q1.size()==0;
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
