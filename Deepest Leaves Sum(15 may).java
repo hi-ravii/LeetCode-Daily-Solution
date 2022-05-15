4e9e5d72b56afca6036d77a0d10dfefc88f693bd
Ravi Punia <70934188+ravipuniaa@users.noreply.github.com>
2022-05-15 08:31:25 +0530

    Deepest Leaves Sum.java

---
+++ Deepest Leaves Sum(15 may).java
@@ -0,0 +1,50 @@
+// Given the root of a binary tree, return the sum of values of its deepest leaves.
+ 
+
+// Example 1:
+
+
+// Input: root = [1,2,3,4,5,null,6,7,null,null,null,null,8]
+// Output: 15
+// Example 2:
+
+// Input: root = [6,7,8,2,7,1,3,9,null,1,4,null,null,null,5]
+// Output: 19
+ 
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [1, 104].
+// 1 <= Node.val <= 100
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int deepestLeavesSum(TreeNode root) {
+     int res = 0, i;
+     LinkedList<TreeNode> q = new LinkedList<TreeNode>();
+     q.add(root);
+     while (!q.isEmpty()) {
+         for (i = q.size() - 1, res = 0; i >= 0; --i) {
+             TreeNode node = q.poll();
+             res += node.val;
+             if (node.right != null) q.add(node.right);
+             if (node.left  != null) q.add(node.left);
+         }
+     }
+     return res;
+ }
+}
