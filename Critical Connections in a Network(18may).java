8b972db263532db938462bb40a1a1defbeff3543
Ravi Punia <70934188+ravipuniaa@users.noreply.github.com>
2022-05-18 08:59:41 +0530

    Critical Connections in a Network (18 May)

---
+++ Critical Connections in a Network(18may).java
@@ -0,0 +1,62 @@
+// There are n servers numbered from 0 to n - 1 connected by undirected server-to-server connections forming a network where connections[i] = [ai, bi] represents a connection between servers ai and bi. Any server can reach other servers directly or indirectly through the network.
+
+// A critical connection is a connection that, if removed, will make some servers unable to reach some other server.
+
+// Return all critical connections in the network in any order.
+
+ 
+
+// Example 1:
+
+
+// Input: n = 4, connections = [[0,1],[1,2],[2,0],[1,3]]
+// Output: [[1,3]]
+// Explanation: [[3,1]] is also accepted.
+// Example 2:
+
+// Input: n = 2, connections = [[0,1]]
+// Output: [[0,1]]
+ 
+
+// Constraints:
+
+// 2 <= n <= 105
+// n - 1 <= connections.length <= 105
+// 0 <= ai, bi <= n - 1
+// ai != bi
+// There are no repeated connections.
+class Solution {
+    // We record the timestamp that we visit each node. For each node, we check every neighbor except its parent and return a smallest timestamp in all its neighbors. If this timestamp is strictly less than the node's timestamp, we know that this node is somehow in a cycle. Otherwise, this edge from the parent to this node is a critical connection
+    public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
+        List<Integer>[] graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
+        
+        for(List<Integer> oneConnection :connections) {
+            graph[oneConnection.get(0)].add(oneConnection.get(1));
+            graph[oneConnection.get(1)].add(oneConnection.get(0));
+        }
+        int timer[] = new int[1];
+        List<List<Integer>> results = new ArrayList<>();
+        boolean[] visited = new boolean[n];
+        int []timeStampAtThatNode = new int[n]; 
+        criticalConnectionsUtil(graph, -1, 0, timer, visited, results, timeStampAtThatNode);
+        return results;
+    }
+    
+    
+    public void criticalConnectionsUtil(List<Integer>[] graph, int parent, int node, int timer[], boolean[] visited, List<List<Integer>> results, int []timeStampAtThatNode) {
+        visited[node] = true;
+        timeStampAtThatNode[node] = timer[0]++;
+        int currentTimeStamp = timeStampAtThatNode[node];
+        
+        for(int oneNeighbour : graph[node]) {
+            if(oneNeighbour == parent) continue;
+            if(!visited[oneNeighbour]) criticalConnectionsUtil(graph, node, oneNeighbour, timer, visited, results, timeStampAtThatNode);
+            timeStampAtThatNode[node] = Math.min(timeStampAtThatNode[node], timeStampAtThatNode[oneNeighbour]);
+            if(currentTimeStamp < timeStampAtThatNode[oneNeighbour]) results.add(Arrays.asList(node, oneNeighbour));
+        }
+        
+        
+    }
+    
+}
