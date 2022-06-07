f75d6b64d1287552035b732178308513388cc911
Ravi Punia <70934188+ravipuniaa@users.noreply.github.com>
2022-06-07 14:52:28 +0530

    Count Complete Tree Nodes

--- Count Complete Tree Nodes.java
+++ Count Complete Tree Nodes.java
@@ -54,3 +54,44 @@ class Solution {
 		return countNodes(root.left) + countNodes(root.right) + 1;
 	}
 }
+APPROACH 2: USING PROPERTY OF COMPLETE TREE
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+        if(root==null)return 0;
+        int left = leftheight(root);
+        int right =rightheight(root);
+        if(left==right) return (2<<left) -1;
+        return countNodes(root.left)+ countNodes(root.right)+1;
+    }
+    public int leftheight(TreeNode root){
+        int count=0;
+        while(root.left!=null){
+            count++;
+            root=root.left;
+        }
+        return count;
+    }
+    public int rightheight(TreeNode root){
+        int count=0;
+        while(root.right!=null){
+            count++;
+            root=root.right;
+        }
+        return count;
+    }
+}
