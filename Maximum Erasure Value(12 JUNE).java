13087f8f89b4ff982d2c78cdbf70fd6a597206f3
Ravi Punia <70934188+ravipuniaa@users.noreply.github.com>
2022-06-12 10:50:11 +0530

    12 JUNE

---
+++ Maximum Erasure Value(12 JUNE).java
@@ -0,0 +1,56 @@
+// You are given an array of positive integers nums and want to erase a subarray containing unique elements. The score you get by erasing the subarray is equal to the sum of its elements.
+
+// Return the maximum score you can get by erasing exactly one subarray.
+
+// An array b is called to be a subarray of a if it forms a contiguous subsequence of a, that is, if it is equal to a[l],a[l+1],...,a[r] for some (l,r).
+
+ 
+
+// Example 1:
+
+// Input: nums = [4,2,4,5,6]
+// Output: 17
+// Explanation: The optimal subarray here is [2,4,5,6].
+// Example 2:
+
+// Input: nums = [5,2,1,2,5,2,1,2,5]
+// Output: 8
+// Explanation: The optimal subarray here is [5,2,1] or [1,2,5].
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// 1 <= nums[i] <= 104
+// Accepted
+// 54.8K
+// Submissions
+// 99.8K
+
+class Solution {
+    public int maximumUniqueSubarray(int[] nums) {
+        
+        Set<Integer> set =  new HashSet();
+        
+      int sum =0, ans =0;
+        int j = 0;
+        
+        int i = 0;
+        
+        while(i<nums.length && j<nums.length){
+            
+            if(!set.contains(nums[j])){
+                
+                sum+=nums[j];
+                ans = Math.max(sum,ans);
+                set.add(nums[j++]);
+            }else{
+                
+                sum = sum-nums[i];
+                set.remove(nums[i++]);
+            }
+        }
+
+        return ans;
+    }
+}
