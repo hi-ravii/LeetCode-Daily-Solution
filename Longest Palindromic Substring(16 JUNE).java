542c1f1d007b8a3cac6db7da6a6ff236c8003302
Ravi Punia <70934188+ravipuniaa@users.noreply.github.com>
2022-06-16 17:44:41 +0530

    16 june

---
+++ Longest Palindromic Substring(16 JUNE).java
@@ -0,0 +1,46 @@
+// Given a string s, return the longest palindromic substring in s.
+
+// Example 1:
+
+// Input: s = "babad"
+// Output: "bab"
+// Explanation: "aba" is also a valid answer.
+// Example 2:
+
+// Input: s = "cbbd"
+// Output: "bb"
+
+// Constraints:
+
+// 1 <= s.length <= 1000
+// s consist of only digits and English letters.
+// Accepted
+// 1,904,086
+// Submissions
+// 5,942,012
+public class Solution {
+    private int lo, maxLen;
+
+    public String longestPalindrome(String s) {
+        int len = s.length();
+        if (len < 2)
+            return s;
+
+        for (int i = 0; i < len - 1; i++) {
+            extendPalindrome(s, i, i); // assume odd length, try to extend Palindrome as possible
+            extendPalindrome(s, i, i + 1); // assume even length.
+        }
+        return s.substring(lo, lo + maxLen);
+    }
+
+    private void extendPalindrome(String s, int j, int k) {
+        while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
+            j--;
+            k++;
+        }
+        if (maxLen < k - j - 1) {
+            lo = j + 1;
+            maxLen = k - j - 1;
+        }
+    }
+}
