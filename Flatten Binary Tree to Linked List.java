dfc06fd456db183556ae65a858d4f28e6f007fa5
Ravi Punia <70934188+ravipuniaa@users.noreply.github.com>
2022-06-18 23:45:33 +0530

    Flatten Binary Tree to Linked List

--- Flatten Binary Tree to Linked List.java
+++ Flatten Binary Tree to Linked List.java
@@ -77,3 +77,41 @@ class Solution {
         
     }
 }
+APPROACH 3: MORRIS traversal
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    TreeNode prev = null;
+    public void flatten(TreeNode root) {
+        TreeNode cur = root;
+		while (cur!=null)
+		{
+			if(cur.left!=null)
+			{
+				TreeNode pre = cur.left;
+				while(pre.right!=null)
+				{
+					pre = pre.right;
+				}
+				pre.right = cur.right;
+				cur.right = cur.left;
+				cur.left = null;
+			}
+			cur = cur.right;
+		}
+        
+    }
+}
