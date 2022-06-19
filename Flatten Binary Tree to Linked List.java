9c55a0a95fdb928b882e591f0b4d0229db764252
Ravi Punia <70934188+ravipuniaa@users.noreply.github.com>
2022-06-19 10:09:15 +0530

    Search in a Binary Search Tree.JAVA

--- Flatten Binary Tree to Linked List.java
+++ Flatten Binary Tree to Linked List.java
@@ -1,117 +1,16 @@
-// Given the root of a binary tree, flatten the tree into a "linked list":
-
-// The "linked list" should use the same TreeNode class where the right child pointer points to the next node in the list and the left child pointer is always null.
-// The "linked list" should be in the same order as a pre-order traversal of the binary tree.
- 
-
-// Example 1:
-
-
-// Input: root = [1,2,5,3,4,null,6]
-// Output: [1,null,2,null,3,null,4,null,5,null,6]
-// Example 2:
-
-// Input: root = []
-// Output: []
-// Example 3:
-
-// Input: root = [0]
-// Output: [0]
- 
-
-// Constraints:
-
-// The number of nodes in the tree is in the range [0, 2000].
-// -100 <= Node.val <= 100
- 
-
-// Follow up: Can you flatten the tree in-place (with O(1) extra space)?
-// Accepted
-// 616,393
-// Submissions
-// 1,057,130
-APPROACH 1 : RIGHT LEFT ROOT RECURRSION
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    TreeNode prev = null;
-    public void flatten(TreeNode root) {
-        if(root== null)return ;
-        flatten(root.right);
-        flatten(root.left);
-        root.right=prev;
-        root.left= null;
-        prev = root;
+boolean[] up = new boolean[26];
+boolean[] down = new boolean[26];
+char[] arr = str.toCharArray();
+for(char c: arr){
+    if(Character.isLowerCase(c)){
+        down[c-'a'] = true;
     }
-}
-APPROACH :2 - ITRATIVE USING STACK
-class Solution {
-    TreeNode prev = null;
-    public void flatten(TreeNode root) {
-        Stack<TreeNode> st = new Stack<>();
-        if(root==null)return;
-        st.push(root);
-        while(!st.isEmpty()){
-            TreeNode curr =st.peek();
-            st.pop();
-            if(curr.right!=null) st.push(curr.right);
-            if(curr.left!=null) st.push(curr.left);
-            if(!st.isEmpty()){
-                curr.right = st.peek();
-                curr.left = null;
-            }
+    if(Character.isUpperCase(c)){
+        up[c-'A'] = true;
+    }
+    for(int i =25;i>=0;i--){
+        if(up[i] && down[i]){
+            return (char)('A'+i)+ "";
         }
-        
     }
-}
-APPROACH 3: MORRIS traversal
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    TreeNode prev = null;
-    public void flatten(TreeNode root) {
-        TreeNode cur = root;
-		while (cur!=null)
-		{
-			if(cur.left!=null)
-			{
-				TreeNode pre = cur.left;
-				while(pre.right!=null)
-				{
-					pre = pre.right;
-				}
-				pre.right = cur.right;
-				cur.right = cur.left;
-				cur.left = null;
-			}
-			cur = cur.right;
-		}
-        
-    }
-}
+        return "";
--- Search in a Binary Search Tree(14April).java
+++ Search in a Binary Search Tree(14April).java
@@ -37,14 +37,25 @@
  *     }
  * }
  */
+APPROACH 1 : USING BST PROPERTY
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
      //iterative method 
         if(root == null)return null;
         while (root != null && root.val !=val){
-            root = val <root.val?root.left :root.right;
+            if(val<root.val)root=root.left;
+            else root=root.right;
         }
         return root;
         
     }
 }
+APPROACH 2 : USING RECURSION
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root == null)return null;
+        if(root.val == val)return root;
+        if(val<root.val)
+            return searchBST(root.left,val);
+        else return searchBST(root.right,val);
+    }
