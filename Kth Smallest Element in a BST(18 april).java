dd65d44521e2ef1553a678ad98a8b62115ab4c62
Ravi Punia <70934188+ravipuniaa@users.noreply.github.com>
2022-06-21 21:47:15 +0530

    Kth Smallest Element in a BST

--- Kth Smallest Element in a BST(18 april).java
+++ Kth Smallest Element in a BST(18 april).java
@@ -1,18 +1,13 @@
 // Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
 
- 
-
 // Example 1:
-
 
 // Input: root = [3,1,4,null,2], k = 1
 // Output: 1
 // Example 2:
 
-
 // Input: root = [5,3,6,2,4,null,null,1], k = 3
 // Output: 3
- 
 
 // Constraints:
 
@@ -22,44 +17,64 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
-class Solution 
-{
-    public void inorder(TreeNode root, int k, int[] res)//BST inorder is ascendingly sorted 
+class Solution {
+    public void inorder(TreeNode root, int k, int[] res)// BST inorder is ascendingly sorted
     {
-        if(root == null)//base case when we reach to null node we return to the parent by deleting the current activation block 
+        if (root == null)// base case when we reach to null node we return to the parent by deleting the
+                         // current activation block
             return;
-        
-        inorder(root.left, k, res);//Recursing the Left subtree
-        
-        res[0]= res[0] + 1;//increamenting the index as we are visiting the node in a sorted fashion //Root
-        
-        if(res[0] == k){//when the desired index is found 
-            res[1]= root.val;//overwridding the value, with the value present at kth index//-1 is for index out of bound 
-            return;//saving the space and not traversing the whole tree, as the desired index element is found 
+
+        inorder(root.left, k, res);// Recursing the Left subtree
+
+        res[0] = res[0] + 1;// increamenting the index as we are visiting the node in a sorted fashion
+                            // //Root
+
+        if (res[0] == k) {// when the desired index is found
+            res[1] = root.val;// overwridding the value, with the value present at kth index//-1 is for index
+                              // out of bound
+            return;// saving the space and not traversing the whole tree, as the desired index
+                   // element is found
         }
-        
-        inorder(root.right, k, res);//Recursing the Right subtree
-        
-        return;//deleting the current activation block as all the function are completed, returning to the callinng function 
+
+        inorder(root.right, k, res);// Recursing the Right subtree
+
+        return;// deleting the current activation block as all the function are completed,
+               // returning to the callinng function
     }
-    public int kthSmallest(TreeNode root, int k) 
-    {
-        int[] res= {0, -1};
+
+    public int kthSmallest(TreeNode root, int k) {
+        int[] res = { 0, -1 };
+
+        inorder(root, k, res);// calling the inorder to get the samllest kth element
+
+        return res[1];// if the index is out of bound then -1 is returned, else the value is returned
+    }
+}
+
+APPROACH 2:
+class Solution {
+    int ans = 0;
+    int count = 0;
+    public int kthSmallest(TreeNode root, int k) {
+        if (root == null) {
+            return 0;
+        }
+        kthSmallest(root.left, k);
+        count++;
+        kthSmallest(root.right, k);
         
-        inorder(root, k, res);//calling the inorder to get the samllest kth element 
-        
-        return res[1];//if the index is out of bound then -1 is returned, else the value is returned 
+        return ans;
     }
 }
