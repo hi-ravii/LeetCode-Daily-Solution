4d1b2ab63e7751d11d219c2883d21792a7be4668
Ravi Punia <70934188+ravipuniaa@users.noreply.github.com>
2022-06-21 18:47:43 +0530

    Delete Node in a BST

---
+++ Delete Node in a BST.java
@@ -0,0 +1,124 @@
+// Given a root node reference of a BST and a key, delete the node with the given key in the BST. Return the root node reference (possibly updated) of the BST.
+
+// Basically, the deletion can be divided into two stages:
+
+// Search for a node to remove.
+// If the node is found, delete the node.
+ 
+
+// Example 1:
+
+
+// Input: root = [5,3,6,2,4,null,7], key = 3
+// Output: [5,4,6,2,null,null,7]
+// Explanation: Given key to delete is 3. So we find the node with value 3 and delete it.
+// One valid answer is [5,4,6,2,null,null,7], shown in the above BST.
+// Please notice that another valid answer is [5,2,6,null,4,null,7] and it's also accepted.
+
+// Example 2:
+
+// Input: root = [5,3,6,2,4,null,7], key = 0
+// Output: [5,3,6,2,4,null,7]
+// Explanation: The tree does not contain a node with value = 0.
+// Example 3:
+
+// Input: root = [], key = 0
+// Output: []
+ 
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [0, 104].
+// -105 <= Node.val <= 105
+// Each node has a unique value.
+// root is a valid binary search tree.
+// -105 <= key <= 105
+ 
+
+// Follow up: Could you solve it with time complexity O(height of tree)?
+
+// Accepted
+// 284,999
+// Submissions
+// 578,465
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root==null) return null;
+        if(root.val == key) return helper(root);
+        TreeNode dummy = root;
+        while(root!=null){
+            if(root.val>key){
+                if(root.left!=null&&root.left.val==key){
+                    root.left = helper(root.left);    
+                    break;
+                }
+                else{
+                    root=root.left;
+                }
+            }
+            else{
+                if(root.right!=null&&root.right.val==key){
+                    root.right = helper(root.right);   
+                    break;
+                }
+                else{
+                    root=root.right;
+                }
+            }     
+        }
+          
+    
+        return dummy;
+    }
+    public TreeNode helper(TreeNode root){
+        if(root.left==null)return root.right;
+        else if(root.right==null)return root.left;
+        else{
+            TreeNode rightChild = root.right;
+            TreeNode rightmostofLeftSubtree = findright(root.left);
+            rightmostofLeftSubtree.right=rightChild;
+            return root.left;
+        }
+        
+    }
+        public TreeNode findright(TreeNode root){
+            if(root.right==null)return root;
+            else return findright(root.right);
+            
+        }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
