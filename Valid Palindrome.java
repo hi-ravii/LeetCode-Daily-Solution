83ba3188698441c41e67c4617ad423b219cf0d5a
Ravi Punia <70934188+ravipuniaa@users.noreply.github.com>
2022-07-10 17:51:14 +0530

    Valid Palindrome

---
+++ Valid Palindrome.java
@@ -0,0 +1,76 @@
+// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+// Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
+
+// Example 1:
+
+// Input: s = "A man, a plan, a canal: Panama"
+// Output: true
+// Explanation: "amanaplanacanalpanama" is a palindrome.
+// Example 2:
+
+// Input: s = "race a car"
+// Output: false
+// Explanation: "raceacar" is not a palindrome.
+// Example 3:
+
+// Input: s = " "
+// Output: true
+// Explanation: s is an empty string "" after removing non-alphanumeric characters.
+// Since an empty string reads the same forward and backward, it is a palindrome.
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 2 * 105
+// s consists only of printable ASCII characters.
+// Accepted
+// 1,379,343
+// Submissions
+// 3,251,492
+APPROACH 1
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder sb = new StringBuilder();
+        for(int i =0;i<s.length();i++){
+            char ch = s .charAt(i);
+            if(Character.isLetterOrDigit(ch)){
+                sb.append(Character.toLowerCase(ch));
+            }
+            
+        }
+        return isValid(sb.toString());
+    }
+    public boolean isValid(String sb){
+        int i =0,j=sb.length()-1;
+        while(i<=j){
+            if(sb.charAt(i)!=sb.charAt(j))return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
+APPROACH 2
+class Solution {
+    public boolean isPalindrome(String s) {
+        int start = 0;
+        int end = s.length() - 1;
+        
+        while(start <= end){
+            while(start <= end && !Character.isLetterOrDigit(s.charAt(start))){
+                start++;
+            }
+            while(start <= end && !Character.isLetterOrDigit(s.charAt(end))){
+                end--;
+            }
+            if(start <= end && Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))) return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
--- contest.java
+++ contest.java
@@ -1,50 +1,30 @@
-// You are given two integers m and n, which represent the dimensions of a matrix.
-
-// You are also given the head of a linked list of integers.
-
-// Generate an m x n matrix that contains the integers in the linked list presented in spiral order (clockwise), starting from the top-left of the matrix. If there are remaining empty spaces, fill them with -1.
-
-// Return the generated matrix.
-// Example 1:
-
-
-// Input: m = 3, n = 5, head = [3,0,2,6,8,1,7,9,4,2,5,5,0]
-// Output: [[3,0,2,6,8],[5,0,-1,-1,1],[5,2,4,9,7]]
-// Explanation: The diagram above shows how the values are printed in the matrix.
-// Note that the remaining spaces in the matrix are filled with -1.
-// Example 2:
-
-
-// Input: m = 1, n = 4, head = [0,1,2]
-// Output: [[0,1,2,-1]]
-// Explanation: The diagram above shows how the values are printed from left to right in the matrix.
-// The last space in the matrix is set to -1.
- 
-
-// Constraints:
-
-// 1 <= m, n <= 105
-// 1 <= m * n <= 105
-// The number of nodes in the list is in the range [1, m * n].
-// 0 <= Node.val <= 1000
 class Solution {
-    public int[][] spiralMatrix(int m, int n, ListNode head) {
-        int matrix[][] =new matrix[m][n];
-        int i=0,j=0;
-        while(head!=null){
-            matrix[i][j]=head.val;
-            head=head.next;
-            if(j==n-1){
-                i++;
-                j--;
-            }
-            else if(i==m-1){
-                j--;
-            }
-            else{
-                i++;
-            }
-        }
-        return matrix;
-    }
+public:
+    int latestTimeCatchTheBus(vector<int>& buses, vector<int>& passengers, int capacity) {
+          sort(buses.begin(), buses.end());
+           sort(passengers.begin(), passengers.end());
+          set<int> v;
+           int j=0, cnt=0, last=0;
+                        for (int i = 0; i < buses.size(); i++) {
+                            cnt=0;
+
+while(cnt<capacity && j<passengers.size()&& passengers[j] <= buses[i]) {
+    last= passengers[j];
+    v.insert(passengers[j++]);
+    cnt++;
 }
+if(i == buses.size() - 1 && cnt< capacity && last< buses [i])
+    return buses.back();
+                        }
+if(v.size()==0) return buses.back();
+
+auto iter= v.rbegin();
+
+int mx=*iter;
+
+while(v.find(mx)!=v.end()) mx--;
+
+return mx;
+    
+    }
+};
