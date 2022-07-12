b26bdd87f656e1f5268672d8256ea136127f0086
Ravi Punia <70934188+ravipuniaa@users.noreply.github.com>
2022-07-12 18:02:33 +0530

    Daily Temperatures

--- Daily Temperatures.java
+++ Daily Temperatures.java
@@ -44,3 +44,19 @@ class Solution {
         return res;
     }
 }
+APPRACH 2: BRUTE FORCE
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n =temperatures.length;
+        int res []=new int[n];
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(temperatures[j]>temperatures[i]){
+                    res[i]=j-i;
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+}
