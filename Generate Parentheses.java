9af33571217d063272cdca77ad4d3a6ef36adc0f
Ravi Punia <70934188+ravipuniaa@users.noreply.github.com>
2022-09-20 22:19:12 +0530

    Generate Parentheses

--- Generate Parentheses.java
+++ Generate Parentheses.java
@@ -23,24 +23,25 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> res = new ArrayList<>();
-        int open =n;
-        int close = n;
-        String op = "";
-        solve(open,close,op,res);
+        int open =n;//no. of open brakets
+        int close = n;//no. of close brakets
+        String op = "";//output string
+        solve(open,close,op,res);//calling the helper function
         return res;
     }
     public void solve(int open,int close,String op,List<String> res){
-        if(open==0&&close==0)res.add(op);
+        //base case
+        if(open==0&&close==0)res.add(op);//if no open and close brakets are left then add the string to the list and return 
         
-        if(open!=0){
-            String op1 = op;
-            op1+='(';
-            solve(open-1,close,op1,res);
+        if(open!=0){//we can add open braket at any time in the string
+            String op1 = op;//creating a new string to store the string with open braket added
+            op1+='(';//adding open braket
+            solve(open-1,close,op1,res);//calling the function with one less open braket
         }
-        if(close>open){
-            String op2 = op;
-            op2+=')';
-            solve(open,close-1,op2,res);
+        if(close>open){//we can add close braket only if there are more close brakets than open brakets otherwise it will be invalid string
+            String op2 = op;//creating a new string to store the string with close braket added
+            op2+=')';//adding close braket
+            solve(open,close-1,op2,res);//calling the function with one less close braket
         }
     }
 }
