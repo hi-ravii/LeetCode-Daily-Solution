1753a1042c3c4f970c509dbb7edc32c3d33374b1
Ravi Punia <70934188+ravipuniaa@users.noreply.github.com>
2022-09-21 19:20:17 +0530

    Merge Two Sorted Lists

--- Merge Two Sorted Lists.java
+++ Merge Two Sorted Lists.java
@@ -40,6 +40,28 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+APPRACH 1 : ITERATIVE
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode prev = dummy;
+        while(l1!=null && l2!=null){
+            if(l1.val<=l2.val){
+                prev.next = l1;
+                l1 = l1.next;
+            }else{
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+        if(l1!=null)prev.next = l1;
+        if(l2!=null)prev.next = l2;
+        return dummy.next;
+    }
+}
+APPROACH 2 : RECURSIVE
+
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         if(list1==null)return list2;
