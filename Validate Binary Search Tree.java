779aa8d6cc59e4549a4aedb61e51d3e9d83f3231
Ravi Punia <70934188+ravipuniaa@users.noreply.github.com>
2022-09-24 22:28:53 +0530

    Validate BST

--- Validate Binary Search Tree.java
+++ Validate Binary Search Tree.java
@@ -55,3 +55,15 @@ class Solution {
         else return isValidBST(root.left,minVal,root.val)&& isValidBST(root.right,root.val,maxVal);
     }
 }
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return helper(root, Long.MAX_VALUE,Long.MIN_VALUE);
+    }
+    public boolean helper(TreeNode root, long max,long min){
+        if(root==null)return true;
+        if(root.val>=max||root.val<=min)return false;
+        boolean left = helper(root.left,root.val,min);
+        boolean right = helper(root.right,max,root.val);
+        return left&&right;
+    }
+}
