006d93b2313aef56bd7bcf175cbf47d003dfcfb3
Ravi Punia <70934188+ravipuniaa@users.noreply.github.com>
2022-09-27 17:47:04 +0530

    Top View Of BT

--- Top view of a Binary Tree.java
+++ Top view of a Binary Tree.java
@@ -32,39 +32,121 @@
 // Otherwise store the node->left and node->right along with there line nos. to the queue.
 // Then print the node->val from the map
 // Code:
+
+// Time Complexity: O(N)
+
+// Space Complexity: O(N)
+
+
+
+//User function Template for Java
+
+/*
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+*/
+
+
+//User function Template for Java
+
+/*
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+*/
+//rest explanation is in copy
 class Solution
 {
+    static class Pair{
+        Node root;
+        int line;
+        Pair(Node root,int line){
+            this.root=root;
+            this.line=line;
+        }
+        
+    }
     //Function to return a list of nodes visible from the top view 
     //from left to right in Binary Tree.
     static ArrayList<Integer> topView(Node root)
+
     {
-        ArrayList<Integer> ans = new ArrayList<>(); 
-        if(root == null) return ans;
-        Map<Integer, Integer> map = new TreeMap<>();
-        Queue<Pair> q = new LinkedList<Pair>();
-        q.add(new Pair(root, 0)); 
-        while(!q.isEmpty()) {
-            Pair it = q.remove();
-            int hd = it.hd; 
-            Node temp = it.node; 
-            if(map.get(hd) == null) map.put(hd, temp.data); 
-            if(temp.left != null) {
-                
-                q.add(new Pair(temp.left, hd - 1)); 
-            }
-            if(temp.right != null) {
-                
-                q.add(new Pair(temp.right, hd + 1)); 
-            }
+        ArrayList<Integer> res = new ArrayList<>();
+        // add your code
+        if(root==null)return res;
+        Map<Integer,Integer> map = new TreeMap<>();//TreeMap is used to store the data in sorted order of keys 
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(root,0));
+        while(!q.isEmpty()){
+            Pair top =q.remove();//here datatype has become the pair
+            int line = top.line;
+            Node temp = top.root;
+            if(!map.containsKey(line))map.put(line,temp.data);
+            if(temp.left!=null)q.add(new Pair(temp.left,line-1));
+            if(temp.right!=null)q.add(new Pair(temp.right,line+1));
         }
-    
-        for (Map.Entry<Integer,Integer> entry : map.entrySet()) {
-            ans.add(entry.getValue()); 
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()){//here we are iterating through the map and printing the values   
+            res.add(entry.getValue());
         }
-        return ans; 
-        
+        return res;
     }
 }
-Time Complexity: O(N)
 
-Space Complexity: O(N)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
