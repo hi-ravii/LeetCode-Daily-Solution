10fcfed2a3370bacb4aacf777340703cbb010fd3
Ravi Punia <70934188+ravipuniaa@users.noreply.github.com>
2022-10-08 18:01:13 +0530

    Flatten Binary Tree to Linked List

--- Flatten Binary Tree to Linked List.java
+++ Flatten Binary Tree to Linked List.java
@@ -1,16 +1,75 @@
-boolean[] up = new boolean[26];
-boolean[] down = new boolean[26];
-char[] arr = str.toCharArray();
-for(char c: arr){
-    if(Character.isLowerCase(c)){
-        down[c-'a'] = true;
+// Given the root of a binary tree, flatten the tree into a "linked list":
+
+// The "linked list" should use the same TreeNode class where the right child pointer points to the next node in the list and the left child pointer is always null.
+// The "linked list" should be in the same order as a pre-order traversal of the binary tree.
+ 
+
+// Example 1:
+
+
+// Input: root = [1,2,5,3,4,null,6]
+// Output: [1,null,2,null,3,null,4,null,5,null,6]
+// Example 2:
+
+// Input: root = []
+// Output: []
+// Example 3:
+
+// Input: root = [0]
+// Output: [0]
+ 
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [0, 2000].
+// -100 <= Node.val <= 100
+ 
+
+// Follow up: Can you flatten the tree in-place (with O(1) extra space)?
+// Accepted
+// 719,170
+// Submissions
+// 1,180,099
+APPROACH 1 : RECURRSIVE
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    TreeNode prev = null;
+    public void flatten(TreeNode root) {
+        if(root==null)return;
+        flatten(root.right);
+        flatten(root.left);
+        root.right=prev;
+        root.left=null;
+        prev=root;
     }
-    if(Character.isUpperCase(c)){
-        up[c-'A'] = true;
-    }
-    for(int i =25;i>=0;i--){
-        if(up[i] && down[i]){
-            return (char)('A'+i)+ "";
+}
+
+APPROACH 2 : ITERATIVE
+class Solution {
+    public void flatten(TreeNode root) {
+        if(root==null)return;
+        Stack<TreeNode> st = new Stack<>();
+        st.push(root);
+        while(!st.isEmpty()){
+            TreeNode curr = st.pop();
+            if(curr.right!=null)st.push(curr.right);
+            if(curr.left!=null)st.push(curr.left);
+            if(!st.isEmpty())curr.right=st.peek();
+            curr.left=null;
         }
     }
-        return "";
+}
